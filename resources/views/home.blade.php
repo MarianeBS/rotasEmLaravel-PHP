ab8a71db5c0485f8a41cde290e5995e15726440c
MarianeBS <mariane.souza22@etec.sp.gov.br>
2023-02-24 19:38:34 -0300

    Utilizando Rotas

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Home</h1><br>
+    <a href="/">Home</a><br>
+    <a href="/produtos">Produtos</a><br>
+    <a href="/empresa">Empresa</a><br>
+    <a href="/contato">Contato</a>
+</body>
+</html>
